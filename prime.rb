539e2e39524abc58416e0b00928e3a446dc0458c
Slobako <slobkov02@yahoo.com>
2016-04-11 20:08:21 +0000

    nitrous-backup at Mon Apr 11 20:08:21 UTC 2016

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(n)# Add  code here!
+  first_primes = [0, 1, 2, 3, 5, 7]
+  if (n % 2 != 0) && (n % 3 != 0) && (n % 5 != 0) && (n % 7 !=0) || for index = (0..5) n = first_primes(index) end
+    true
+  else
+    false
+  end
+end
+
